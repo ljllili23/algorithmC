2a5742eaf4fe18a823bdac54e3232c55b47adc9e
ljllili23 <mac@macdeMacBook-Air-2.local>
2018-02-12 22:02:13 +0800

    modified

--- Dijkstra_01.cpp
+++ Dijkstra_01.cpp
@@ -8,17 +8,19 @@ struct E{
 	int next;
 	int cost;
 };
+
 vector<E> edge[101];
 bool mark[101];
 int Dist[101];
+
 int main(){
 	int n,m;
 	while(scanf("%d%d",&n,&m)!=EOF){
-		if(n==0 && m==0) break;
+		if(n==0 || m==0) break;
 		for(int i=1;i<=n;++i){
+			mark[i] = false;
+			Dist[i] = -1;
 			edge[i].clear();
-			mark[i]=false;
-			Dist[i]=-1;
 		}
 		while(m--){
 			int a,b,c;
@@ -30,30 +32,29 @@ int main(){
 			tmp.next = a;
 			edge[b].push_back(tmp);
 		}
-		int newP = 1;
 		mark[1]=true;
 		Dist[1]=0;
+		int newP = 1;
 		for(int i=1;i<n;++i){
 			for(int j=0;j<edge[newP].size();++j){
-				int t=edge[newP][j].next;
-				int c=edge[newP][j].cost;
+				int t = edge[newP][j].next;
+				int c = edge[newP][j].cost;
 				if(mark[t]==true) continue;
 				if(Dist[t]==-1 || Dist[t]>Dist[newP]+c)
 					Dist[t] = Dist[newP]+c;
 			}
-			int min = 123123123;
 			for(int j=1;j<=n;++j){
 				if(mark[j]==true) continue;
 				if(Dist[j]==-1) continue;
-				if(Dist<min){
+				int min = 123123123;
+				if(Dist[j]<min){
 					min = Dist[j];
 					newP = j;
 				}
 			}
-			mark[newP]=true;
+			mark[newP] = true;
 		}
 		printf("%d\n",Dist[n]);
-
 	}
 	return 0;
 }
