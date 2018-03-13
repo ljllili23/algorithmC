38c69b3c153b242fba914f1384380ad706906d90
ljllili23 <ljllili23@outlook.com>
2018-03-13 10:42:50 +0800

    BT-AC

--- ANOJ/BT_01.cpp
+++ ANOJ/BT_01.cpp
@@ -9,60 +9,63 @@ struct Node{
 }Tree[100001];
 int loc;
 Node* creat(){
-	Tree[loc].lchild=Tree[loc].rchild=NULL;
+	Tree[loc].lchild = Tree[loc].rchild = NULL;
 	return &Tree[loc++];
 }
 
-void Insert(Node* p,int x){
-	if(p==NULL){
-		p=creat();
-		p->c=x;
-		//return p;
-	}else if(x>p->c){
-		//p->rchild=Insert(p->rchild,x);
-		Insert(p->rchild,x);
-	}else if(x<p->c){
-		//p->lchild=Insert(p->lchild,x);
-		Insert(p->lchild,x);
+Node* Insert(Node* p, int x){
+	if (p == NULL){
+		p = creat();
+		p->c = x;
+		return p;
 	}
-	//return p;
+	else if (x>p->c){
+		p->rchild=Insert(p->rchild,x);
+		//Insert(p->rchild, x);
+	}
+	else if (x<p->c){
+		p->lchild=Insert(p->lchild,x);
+		//Insert(p->lchild, x);
+	}
+	return p;
 }
 
 void LevelOrder(Node* T){
-	int flag=0;
+	int flag = 0;
 	Node* queue[100001];
-	int front,rear;
-	front=rear=0;
-	queue[rear++]=T;
-	while(rear!=front){
-		Node* t=queue[front++];
-		if(flag==0){
-			cout<<t->c;
-			flag=1;
-		}else{
-			cout<<' '<<t->c;
+	int front, rear;
+	front = rear = 0;
+	queue[rear++] = T;
+	while (rear != front){
+		Node* t = queue[front++];
+		if (flag == 0){
+			cout << t->c;
+			flag = 1;
 		}
-		if(t->lchild!=NULL){
-			queue[rear++]=t->lchild;
+		else{
+			cout << ' ' << t->c;
 		}
-		if(t->rchild!=NULL){
-			queue[rear++]=t->rchild;
+		if (t->lchild != NULL){
+			queue[rear++] = t->lchild;
+		}
+		if (t->rchild != NULL){
+			queue[rear++] = t->rchild;
 		}
 	}
-	cout<<endl;
+	cout << endl;
 }
 
 int main(){
 	int n;
 	//loc=0;
-	while(cin>>n){
-		loc=0;
+	while (cin >> n){
+		loc = 0;
 		int t;
-		Node* Root;
-		while(n--){
-			cin>>t;
+		Node* Root=NULL;
+		while (n--){
+			cin >> t;
 			//Node* tmp;
-			Insert(Root,t);
+			Root = Insert(Root, t);
 		}
 		LevelOrder(Root);
 	}
